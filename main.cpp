bd890b32ca90780d6e34fb923a22d745a4ef6712
Grebtsew <daniel.grebtsew@gmail.com>
2023-07-20 02:11:52 +0200

    Fixed complilation errors, tasks and properties in vscode
    Now the project is ready for coding, c++ dependencies are fixed.

--- main.cpp
+++ main.cpp
@@ -1,8 +1,29 @@
+#include <iostream>
+#include <opencv2/opencv.hpp>
 #include <gst/gst.h>
 
-#include <iostream>
+using namespace cv;
 
 int main() {
+    // Initialize GStreamer
+    gst_init(nullptr, nullptr);
+
+    // Read an image using OpenCV
+    cv::Mat image = cv::imread("./Inputs/Overlays/logo1.png");
+
+    // Check if the image was successfully loaded
+    if (image.empty()) {
+        std::cout << "Failed to load image!" << std::endl;
+        return 1;
+    }
     std::cout << "Hello, World!" << std::endl;
+     std::cout << "Hello, World! asdaiga" << std::endl;
+    // Display the image using OpenCV
+    cv::imshow("Image", image);
+    cv::waitKey(0);
+
+    // Cleanup and shutdown GStreamer
+    gst_deinit();
+
     return 0;
 }
